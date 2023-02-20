f1a57d8b097bbf1aa8b10c78d5c8f6a04e5be689
marlisonmourao <bentesmourao@gmail.com>
2023-02-20 00:38:27 -0400

    project 07

---
+++ Algoritnos e Logica de Programação/07/script.js
@@ -0,0 +1,30 @@
+const patients = [
+  {
+    name: "Marlison",
+    age: 22,
+    weight: 90,
+    height: 165,
+  },
+  {
+    name: "Marcos",
+    age: 40,
+    weight: 70,
+    height: 178,
+  },
+  {
+    name: "Vanessa",
+    age: 29,
+    weight: 67,
+    height: 155,
+  },
+];
+
+let patientsName = []
+
+for(let patient of patients) {
+ patientsName.push(patient.name)
+
+ alert(`${patient.name} tem ${patient.age} anos, pesa ${patient.weight} e tem ${patient.height} de altura`)
+}
+
+alert(patientsName)
