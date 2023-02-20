420cf14246dd40e89715ce2355047e326652a6a5
marlisonmourao <bentesmourao@gmail.com>
2023-02-19 23:20:32 -0400

    random number

---
+++ Algoritnos e Logica de Programação/05/script.js
@@ -0,0 +1,15 @@
+
+
+let result = prompt("Adivinhe o número que estou pensando? Está entre 0 e 10")
+const randomNumber = Math.round(Math.random() * 10)
+// const match = Number(result) != randomNumber
+
+let xAttempts = 1
+
+while (Number(result) != randomNumber) {
+  result = prompt("Erro, tente novamente");
+
+  xAttempts++
+}
+
+alert(`Parabéns, o número que eu pensei foi ${randomNumber}. você adivinhou em ${xAttempts} tentativas.`);
