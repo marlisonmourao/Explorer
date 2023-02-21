e483cd085f739d7691be2d2f86d066848c27232d
marlisonmourao <bentesmourao@gmail.com>
2023-02-21 18:48:43 -0400

    finished website

--- Stage 04 DOM/Jogo-adivinhacao/script.js
+++ Stage 04 DOM/Jogo-adivinhacao/script.js
@@ -1,7 +1,37 @@
-function handleClick(event) {
-  event.preventDefault();
+const screen1 = document.querySelector(".screen1")
+const screen2 = document.querySelector(".screen2")
+const btnTry = document.querySelector("#btnTry")
+const btnReset = document.querySelector("#btnReset")
 
-  const inputNumber = document.querySelector("#input-number");
+const randomNumber = Math.floor(Math.random() * 10)
+let xAttempts = 1
 
-  console.log(inputNumber.value)
+function handleTryClick(event) {
+  event.preventDefault()
+
+  const inputNumber = document.querySelector("#input-number")
+
+  if (inputNumber.value == randomNumber) {
+    handleToggleScreen()
+
+    screen2.querySelector(
+      "h2"
+    ).innerText = `acetou em ${xAttempts} tentativas`
+  }
+
+  inputNumber.value = ""
+  xAttempts++
 }
+
+function handleResetClick() {
+  handleToggleScreen()
+  xAttempts = 1;
+}
+
+function handleToggleScreen() {
+  screen1.classList.toggle("hide");
+  screen2.classList.toggle("hide");
+}
+
+btnTry.addEventListener("click", handleTryClick)
+btnReset.addEventListener("click", handleResetClick)
