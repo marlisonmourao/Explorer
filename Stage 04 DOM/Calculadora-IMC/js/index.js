818a72cd289b6ebbcd0bb1304f2135fec65d6e3f
marlisonmourao <bentesmourao@gmail.com>
2023-02-22 16:00:11 -0400

    showing error when not typing a number

--- Stage 04 DOM/Calculadora-IMC/js/index.js
+++ Stage 04 DOM/Calculadora-IMC/js/index.js
@@ -1,4 +1,5 @@
 import { Modal } from "./modal.js";
+import { AlertError } from "./alert-error.js";
 
 const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
@@ -10,6 +11,15 @@ form.onsubmit = event => {
   const weight = inputWeight.value
   const height = inputHeight.value
 
+  const showAlertError = notNumber(weight) || notNumber(height)
+
+  if (showAlertError) {
+    AlertError.open()
+    return
+  }
+
+  AlertError.close()
+
   const result = IMC(weight, height)
   const message = `Seu IMC é de ${result}`
 
@@ -19,4 +29,8 @@ form.onsubmit = event => {
 
 function IMC(weight, height) {
   return (weight / ((height / 100) ** 2)).toFixed(2)
+}
+
+function notNumber(value) {
+  return isNaN(value) || value == ""
 }
