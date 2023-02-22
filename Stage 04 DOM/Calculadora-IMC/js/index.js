fb8045f9e7a99c0c01558a1fd119e2c412620f46
marlisonmourao <bentesmourao@gmail.com>
2023-02-22 16:09:11 -0400

    refactoring

--- Stage 04 DOM/Calculadora-IMC/js/index.js
+++ Stage 04 DOM/Calculadora-IMC/js/index.js
@@ -1,36 +1,40 @@
 import { Modal } from "./modal.js";
 import { AlertError } from "./alert-error.js";
 
-const form = document.querySelector('form')
-const inputWeight = document.querySelector('#weight')
-const inputHeight = document.querySelector('#height')
+const form = document.querySelector("form");
+const inputWeight = document.querySelector("#weight");
+const inputHeight = document.querySelector("#height");
 
-form.onsubmit = event => {
+form.onsubmit = (event) => {
   event.preventDefault();
 
-  const weight = inputWeight.value
-  const height = inputHeight.value
+  const weight = inputWeight.value;
+  const height = inputHeight.value;
 
-  const showAlertError = notNumber(weight) || notNumber(height)
+  const weightOrHeightIsNotANumber = notNumber(weight) || notNumber(height);
 
-  if (showAlertError) {
-    AlertError.open()
-    return
+  if (weightOrHeightIsNotANumber) {
+    AlertError.open();
+    return;
   }
 
-  AlertError.close()
+  AlertError.close();
 
-  const result = IMC(weight, height)
-  const message = `Seu IMC é de ${result}`
+  const result = calculateIMC(weight, height);
+  displayResultMessage(result)
+};
 
-  Modal.message.innerText = message
-  Modal.open()
-}
-
-function IMC(weight, height) {
-  return (weight / ((height / 100) ** 2)).toFixed(2)
+function calculateIMC(weight, height) {
+  return (weight / (height / 100) ** 2).toFixed(2);
 }
 
 function notNumber(value) {
-  return isNaN(value) || value == ""
+  return isNaN(value) || value == "";
+}
+
+function displayResultMessage(result) {
+  const message = `Seu IMC é de ${result}`;
+
+  Modal.message.innerText = message;
+  Modal.open();
 }
