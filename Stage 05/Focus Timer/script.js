6602181868ed946c705ea96c30f94728f181c316
marlisonmourao <bentesmourao@gmail.com>
2023-02-23 13:08:49 -0400

    refactoring code

--- Stage 05/Focus Timer/script.js
+++ Stage 05/Focus Timer/script.js
@@ -5,22 +5,43 @@ const buttonSet = document.querySelector('.set')
 const buttonSoundOn = document.querySelector('.sound-on')
 const buttonSoundOff = document.querySelector('.sound-off')
 const minutesDisplay = document.querySelector('.minutes')
-const secondsDisplay = document.querySelector('.seconds');
+const secondsDisplay = document.querySelector('.seconds')
 
-let minutes;
+let minutes
+
+function resetControls() {
+  buttonPlay.classList.remove("hide");
+  buttonPauser.classList.add("hide");
+  buttonSet.classList.remove("hide");
+  buttonStop.classList.add("hide");
+}
+
+function updateTimerDispla(minutes, seconds) {
+  minutesDisplay.innerText = String(minutes).padStart(2, "0")
+  secondsDisplay.innerText = String(seconds).padStart(2, "0")
+}
 
 function countdown() {
   setTimeout(() => {
-    let seconds = Number(secondsDisplay.textContent - 1);
-
-    if (seconds <= 0) {
-      seconds = 59;
-    }
-
-    secondsDisplay.textContent = seconds;
+    let seconds = Number(secondsDisplay.textContent)
+    let minutes = Number(minutesDisplay.textContent)
     
+    updateTimerDispla(minutes, 0)
+    
+    if (minutes <= 0) {
+      resetControls()
+      return
+    }
+    
+    if (seconds <= 0) {
+      seconds = 2
+      --minutes
+    }
+    
+    updateTimerDispla(minutes, String(seconds - 1))
+
     countdown()
-  }, 1000);
+  }, 1000)
 }
 
 function playTime() {
@@ -40,24 +61,20 @@ function pauserTime() {
 buttonPlay.addEventListener('click', playTime)
 buttonPauser.addEventListener('click', pauserTime)
 buttonStop.addEventListener('click', () => {
-  buttonPlay.classList.remove('hide')
-  buttonPauser.classList.add('hide')
-  buttonSet.classList.remove('hide')
-  buttonStop.classList.add('hide')
+  resetControls() 
 })
 
 buttonSoundOff.addEventListener('click', () => {
-  buttonSoundOn.classList.remove('hide');
-  buttonSoundOff.classList.add('hide');
+  buttonSoundOn.classList.remove('hide')
+  buttonSoundOff.classList.add('hide')
 })
 
 buttonSoundOn.addEventListener('click', () => {
-  buttonSoundOn.classList.add('hide');
-  buttonSoundOff.classList.remove('hide');
+  buttonSoundOn.classList.add('hide')
+  buttonSoundOff.classList.remove('hide')
 })
 
 buttonSet.addEventListener('click', () => {
   minutes = prompt('Quantos minutes?')
-
-  minutesDisplay.innerText = minutes
+  updateTimerDispla(minutes, 0)
 })
