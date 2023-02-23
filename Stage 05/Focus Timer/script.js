d3c7d3f4795cde4c35366b698bbd14bd20e1de50
marlisonmourao <bentesmourao@gmail.com>
2023-02-23 01:01:13 -0400

    starting countdown

--- Stage 05/Focus Timer/script.js
+++ Stage 05/Focus Timer/script.js
@@ -3,13 +3,33 @@ const buttonPauser = document.querySelector('.pauser')
 const buttonStop = document.querySelector('.stop')
 const buttonSet = document.querySelector('.set')
 const buttonSoundOn = document.querySelector('.sound-on')
-const buttonSoundOff = document.querySelector(".sound-off");
+const buttonSoundOff = document.querySelector('.sound-off')
+const minutesDisplay = document.querySelector('.minutes')
+const secondsDisplay = document.querySelector('.seconds');
+
+let minutes;
+
+function countdown() {
+  setTimeout(() => {
+    let seconds = Number(secondsDisplay.textContent - 1);
+
+    if (seconds <= 0) {
+      seconds = 59;
+    }
+
+    secondsDisplay.textContent = seconds;
+    
+    countdown()
+  }, 1000);
+}
 
 function playTime() {
   buttonPlay.classList.toggle('hide')
   buttonPauser.classList.toggle('hide')
   buttonSet.classList.add('hide')
   buttonStop.classList.remove('hide')
+
+  countdown()
 }
 
 function pauserTime() {
@@ -27,11 +47,17 @@ buttonStop.addEventListener('click', () => {
 })
 
 buttonSoundOff.addEventListener('click', () => {
-  buttonSoundOn.classList.remove("hide");
-  buttonSoundOff.classList.add("hide");
+  buttonSoundOn.classList.remove('hide');
+  buttonSoundOff.classList.add('hide');
 })
 
-buttonSoundOn.addEventListener("click", () => {
-  buttonSoundOn.classList.add("hide");
-  buttonSoundOff.classList.remove("hide");
-});
+buttonSoundOn.addEventListener('click', () => {
+  buttonSoundOn.classList.add('hide');
+  buttonSoundOff.classList.remove('hide');
+})
+
+buttonSet.addEventListener('click', () => {
+  minutes = prompt('Quantos minutes?')
+
+  minutesDisplay.innerText = minutes
+})
