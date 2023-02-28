573b9c77327e7a945c5fc0ab2b28fa1a95aa13c6
marlisonmourao <bentesmourao@gmail.com>
2023-02-28 01:34:54 -0400

    Handling client- and server-side errors

--- Stage 06/Conceitos Node/api/src/routes/user.routes.js
+++ Stage 06/Conceitos Node/api/src/routes/user.routes.js
@@ -5,18 +5,8 @@ const UsersController = require("../controllers/UsersController")
 const userRoutes = Router()
 
 
-function myMiddleware(request, response, next) {
-  console.log("Você passou pelo middleware")
-
-  next()
-}
-
-
-
-
-
 const usersController = new UsersController()
 
-userRoutes.post("/", myMiddleware, usersController.create)
+userRoutes.post("/", usersController.create)
 
 module.exports = userRoutes
