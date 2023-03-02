a37e76e68eb72cc690475b4573cea76e99a52d82
marlisonmourao <bentesmourao@gmail.com>
2023-03-02 02:53:20 -0400

    implementing the fear of updating user fields

--- Stage 06/Conceitos Node/api/src/routes/user.routes.js
+++ Stage 06/Conceitos Node/api/src/routes/user.routes.js
@@ -8,5 +8,6 @@ const userRoutes = Router()
 const usersController = new UsersController()
 
 userRoutes.post("/", usersController.create)
+userRoutes.put("/:id", usersController.update)
 
 module.exports = userRoutes
