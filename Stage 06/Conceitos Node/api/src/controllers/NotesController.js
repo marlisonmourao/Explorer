4798a4c7b2b87680178161b042a3434d96875d95
marlisonmourao <bentesmourao@gmail.com>
2023-03-03 03:36:15 -0400

    doing fetch with whereIn

--- Stage 06/Conceitos Node/api/src/controllers/NotesController.js
+++ Stage 06/Conceitos Node/api/src/controllers/NotesController.js
@@ -56,12 +56,23 @@ class NotesController {
   }
 
   async index(request, response) {
-    const { user_id, text } = request.query
+    const { user_id, text, tags } = request.query
 
-    const notes = await knex("notes")
-    .where({ user_id })
-    .whereLike("text", `%${text}%`)
-    .orderBy("text");
+    let notes
+
+    if(tags) {
+      const filterTags = tags.split(',').map(tag => tag.trim())
+
+      notes = await knex("tags")
+        .whereIn("name", filterTags)
+
+        
+    } else {
+      notes = await knex("notes")
+        .where({ user_id })
+        .whereLike("text", `%${text}%`)
+        .orderBy("text");
+    }
 
     return response.json(notes)
 
