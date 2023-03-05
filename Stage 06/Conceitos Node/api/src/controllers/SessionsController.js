d9d059ace557ad35ddf16a33f723d1d8fab79318
marlisonmourao <bentesmourao@gmail.com>
2023-03-05 19:33:47 -0400

    validating if the entered password is correct

--- Stage 06/Conceitos Node/api/src/controllers/SessionsController.js
+++ Stage 06/Conceitos Node/api/src/controllers/SessionsController.js
@@ -1,16 +1,23 @@
-const knex = require("../database/knex")
-const AppError = require("../utils/AppError")
+const knex = require("../database/knex");
+const AppError = require("../utils/AppError");
+const { compare } = require("bcryptjs");
 
 class SessionsController {
   async create(request, response) {
-    const { email, password } = request.body
+    const { email, password } = request.body;
 
-    const user = await knex("users").where({ email }).first()
+    const user = await knex("users").where({ email }).first();
 
-    if(!user) {
-      throw new AppError("E-mail e/ou senha incorreta")
+    if (!user) {
+      throw new AppError("E-mail e/ou senha incorreta");
     }
-    
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new AppError("E-mail e/ou senha incorreta", 401);
+    }
+
     return response.json(user);
   }
 }
