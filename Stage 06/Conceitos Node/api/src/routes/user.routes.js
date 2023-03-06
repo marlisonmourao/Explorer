e107e1870277d25ad8b02f3b21fcc7f2945129e5
marlisonmourao <bentesmourao@gmail.com>
2023-03-06 15:56:04 -0400

    moving the image to the temporary folder

--- Stage 06/Conceitos Node/api/src/routes/user.routes.js
+++ Stage 06/Conceitos Node/api/src/routes/user.routes.js
@@ -1,14 +1,21 @@
 const { Router } = require("express")
+const multer = require("multer")
+const uploadConfig = require("../configs/upload")
 
 const UsersController = require("../controllers/UsersController")
 const ensureAuthenticated = require("../Middleware/ensureAuthenticated")
 
 const userRoutes = Router()
 
+const upload = multer(uploadConfig.MULTER);
 
 const usersController = new UsersController()
 
 userRoutes.post("/", usersController.create)
 userRoutes.put("/", ensureAuthenticated, usersController.update)
+userRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), (req, res) => {
+  console.log(req.file.filename)
+  res.json();
+})
 
 module.exports = userRoutes
