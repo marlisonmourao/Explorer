557beb7be18fa6becfd0f1d2c324b4dc7d81ab23
marlisonmourao <bentesmourao@gmail.com>
2023-03-05 20:56:46 -0400

    creating authentication middleware

--- Stage 06/Conceitos Node/api/src/controllers/SessionsController.js
+++ Stage 06/Conceitos Node/api/src/controllers/SessionsController.js
@@ -1,24 +1,33 @@
-const knex = require("../database/knex");
-const AppError = require("../utils/AppError");
-const { compare } = require("bcryptjs");
+const knex = require("../database/knex")
+const AppError = require("../utils/AppError")
+const { compare } = require("bcryptjs")
+
+const authConfig = require("../configs/auth")
+const { sign } = require("jsonwebtoken")
 
 class SessionsController {
   async create(request, response) {
-    const { email, password } = request.body;
+    const { email, password } = request.body
 
-    const user = await knex("users").where({ email }).first();
+    const user = await knex("users").where({ email }).first()
 
     if (!user) {
-      throw new AppError("E-mail e/ou senha incorreta");
+      throw new AppError("E-mail e/ou senha incorreta")
     }
 
-    const passwordMatched = await compare(password, user.password);
+    const passwordMatched = await compare(password, user.password)
 
     if (!passwordMatched) {
-      throw new AppError("E-mail e/ou senha incorreta", 401);
+      throw new AppError("E-mail e/ou senha incorreta", 401)
     }
 
-    return response.json(user);
+    const { secret, expiresIn } = authConfig.jwt
+    const token = sign({}, secret, {
+      subject: String(user.id),
+      expiresIn
+    })
+
+    return response.json({user, token})
   }
 }
 
