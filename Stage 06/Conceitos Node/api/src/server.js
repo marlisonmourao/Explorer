2b951f7fa54fa83f3e06cb6b565dcb285a3cfba2
marlisonmourao <bentesmourao@gmail.com>
2023-03-06 21:06:59 -0400

    finished api

--- Stage 06/Conceitos Node/api/src/server.js
+++ Stage 06/Conceitos Node/api/src/server.js
@@ -3,12 +3,15 @@ require("express-async-errors")
 const express = require('express')
 const migrationsRun = require("./database/sqlite/migrations")
 const AppError = require("./utils/AppError")
+const uploadConfig = require("./configs/upload")
 
 const routes = require("./routes")
 migrationsRun()
 
 const app = express()
 app.use(express.json())
+
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER))
 
 app.use(routes)
 
